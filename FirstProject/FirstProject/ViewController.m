50367975414051fb8bb0f612463d9b956b0efbce
cheng.qiu <bartonqiu@gmail.com>
2022-08-24 19:36:36 +0800

    master commit 1

--- FirstProject/FirstProject/ViewController.m
+++ FirstProject/FirstProject/ViewController.m
@@ -22,6 +22,7 @@
     [self.percentLabel setFrame:CGRectMake(20, 100, CGRectGetWidth(self.view.bounds), 35)];
     [self.view addSubview:self.percentLabel];
     int i = 0;
+    int j = 1;
 }
 
 - (void)didReceiveMemoryWarning {
